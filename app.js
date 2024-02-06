76eb930d5a352cfdaba78cf511479a194ec2fad7
maheshbasnet089 <basnetmanish089@gmail.com>
2024-02-06 21:44:41 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app =  express()
+
+ // Alternative 
+//  const app = require('express')()
+
+
+
+
+app.get("/",(req,res)=>{
+    res.send("bye world ")
+})
+
+
+
+
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log("Nodejs server has started at port 3000")
+})
+
